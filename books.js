3b94b9d831b63ca899221623da50825e0af29f81
yt249 <yt249@gmail.com>
2021-05-07 19:22:11 -0400

    create an express middleware handles the authentication process

--- books.js
+++ books.js
@@ -4,6 +4,27 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+const accessTokenSecret = 'youraccesstokensecret';
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
 const books = [
     {
         "author": "Chinua Achebe",
