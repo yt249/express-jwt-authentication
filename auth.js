3341eeb5611adf9824519fe68ac747fbab9da5ef
yt249 <yt249@gmail.com>
2021-05-07 19:06:12 -0400

    create an user array

--- auth.js
+++ auth.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
+
 app.listen(3000, () => {
     console.log('Authentication service started on port 3000');
 });
