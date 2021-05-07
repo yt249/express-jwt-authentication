3f04d16faf66207a3f37d2d34d20831d7c191c50
yt249 <yt249@gmail.com>
2021-05-07 19:20:39 -0400

    create a book array and request handler

--- books.js
+++ books.js
@@ -4,8 +4,39 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+];
+
 app.use(bodyParser.json());
 
 app.listen(4000, () => {
     console.log('Books service started on port 4000');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
 });
