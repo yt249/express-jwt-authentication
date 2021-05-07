62bb3dfbe79e68c04a61adea574f967ebbaae3c8
yt249 <yt249@gmail.com>
2021-05-07 19:07:38 -0400

    create a request handler

--- auth.js
+++ auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const accessTokenSecret = 'youraccesstokensecret';
+
+app.use(bodyParser.json());
 
 const users = [
     {
@@ -15,4 +21,23 @@ const users = [
 
 app.listen(3000, () => {
     console.log('Authentication service started on port 3000');
+});
+
+app.post('/login', (req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
 });
