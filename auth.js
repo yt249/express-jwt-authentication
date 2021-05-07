8bdc9d771d8d12c71161ed1f009b6834f56dc87e
yt249 <yt249@gmail.com>
2021-05-07 19:32:12 -0400

    create a request handler that generated new tokens

--- auth.js
+++ auth.js
@@ -48,3 +48,27 @@ app.post('/login', (req, res) => {
         res.send('Username or password incorrect');
     }
 });
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
